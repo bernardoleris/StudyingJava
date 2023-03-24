794d7af36c3ba61fcb84bfe806c418ca4ba3ea8d
Bernardo Leris <bernardo.leris1@gmail.com>
2023-03-24 01:06:27 -0300

    Estudando arquivos, métodos de arrays, filas e pilhas.

--- ClasseMainTeste.java
+++ ClasseMainTeste.java
@@ -1,28 +1,18 @@
-import TesteVeiculos.Aviao;
-
+import java.nio.file.*;
+import java.util.List;
 public class ClasseMainTeste {
     public static void main(String[] args){
-        /*Carro c1 = new Carro("HRV");
-        Carro c2 = new Carro("Troller");
-        CarroCombate c3 = new CarroCombate("Pantera", 100);
-        CarroCombate c4 = new CarroCombate("Rinoceronte", 50);
-
-        c1.info();
-        c2.info();
-        c3.info();
-        c4.info();
-
-        c3.atirar();
-        c3.atirar();
-        c3.atirar();
-
-        c4.sofrerDano(10);
-        c1.sofrerDano(5);
+        Path arquivo = Paths.get("C:/Users/Bernardo Leris/Desktop/StudyingJava/texto.txt");
         
-        c1.info();
-        c3.info();
-        c4.info(); */
-        Aviao v1 = new Aviao("Voador", 1);
-        v1.info();
+        try{
+            List<String> linhas = Files.readAllLines(arquivo);
+            for(String linha:linhas){
+                System.out.println(linha);
+            }
+        }catch(Exception e){
+            System.out.println("ERRO");
+        }
+
+
     }
 }
---
+++ MetodoString.java
@@ -0,0 +1,21 @@
+public class MetodoString {
+    /*
+    length: tamanho da string
+    charAt: caracter na posição específica
+    getChars: copia caracteres de posições específicas para outro vetor/string
+    equals: comparar duas strings, é diferente de ==
+    equalsIgnoreCase: comparação ignorando se as letras são maiúsculas ou minúsculas
+    compareTo: compara o tamanho das strings, se forem iguais vai retornar 0, se a primeira for menor vai retronar -1, se a segunda foir menor vai retornar 1
+    regionMatches: comparação de parte da String
+    startsWith e endsWith: vai comparar se a string começa ou inicia com a letra ou palavra específica
+    indexOf: localizar um caracter e retornar a posição desse caracter
+    substring: vai retonrar parte de uma string
+    concat: vai juntar duas strings
+    replace: substituir determinado caracter em uma string
+    toLowerCase/toUpperCase: transformar todos os caracteres de uma String em minúsculo ou maiúsculo.
+    trim: remove todos os espaços que estão na frente ou no final de uma String
+    toCharArray: converter a String para um vetor de chars
+    split: vai dividir a string em um ponto definido 
+
+    */
+}
